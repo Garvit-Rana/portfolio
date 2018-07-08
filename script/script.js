c45b0cc3dd5881f8eecd6008047865efc0b67682
Garvit-Rana <garvitrana2000@gmail.com>
2018-07-08 18:01:55 +0530

    added some colors

---
+++ script/script.js
@@ -0,0 +1,105 @@
+//document.addEventListener('click', function()
+//	{
+
+//console.log(event);
+	 //window.alert('clickis here');
+	  
+
+
+//	  });
+//var body=document.querySelector('.two');
+//console.log(body);
+//var a=0;var i=0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var a=10;
+var b;
+var c=document.querySelector('#z')
+c.addEventListener('click',function(){
+
+
+
+	var d=setInterval(function(){ 
+	if (a<100) 
+		window.scrollBy(0,75); 
+	else 
+		clearInterval(d); 
+	a+=9;  }
+                  ,35 )
+
+a=10;
+}
+
+ )
+a=10;
+c=document.querySelector('#c')
+c.addEventListener('click',function()
+{
+d=setInterval(function(){
+if (a<100)
+window.scrollBy(0,84);
+else
+clearInterval(d);
+a+=5;
+},50)
+
+
+a=10;
+
+	})
+a=10;
+c=document.querySelector('#v')
+c.addEventListener('click',function(){
+d=setInterval(function(){
+if (a<100) 
+	window.scrollBy(0,125);
+else
+	clearInterval(d);
+a+=5;
+},50)
+
+a=10;
+
+})
+a=10;
+c=document.querySelector('#b')
+
+c.addEventListener('click',function(){
+d=setInterval(function(){
+if (a<100)
+	window.scrollBy(0,180);
+else
+	clearInterval(d);
+a+=5;
+},50)
+
+a=10;
+})
+
+
+
+
+
+
+
+
+
